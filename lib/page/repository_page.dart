5101bc6cae2adf1e14183b496f0b2628391b3a2c
Angelo Pareto <angelopareto98@gmail.com>
2023-05-07 14:59:25 +0300

    list repository done

--- lib/page/repository_page.dart
+++ lib/page/repository_page.dart
@@ -1,18 +1,54 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class GitRepoPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class GitRepoPage extends StatefulWidget {
   String login;
 
   GitRepoPage({required this.login});
 
   @override
+  State<GitRepoPage> createState() => _GitRepoPageState();
+}
+
+class _GitRepoPageState extends State<GitRepoPage> {
+  dynamic dataRepo;
+
+  @override
+  void initState() {
+    // super.initState();
+    _loadRepository();
+  }
+
+  Future<void> _loadRepository() async {
+    String url = "https://api.github.com/users/${widget.login}/repos";
+
+    var response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        dataRepo = json.decode(response.body);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Repository ${login}"),
+        title: Text("Repository ${widget.login}"),
       ),
       body: Center(
-        child: Text("Repository ${login}"),
+        child: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: Text("${dataRepo[index]['name']}"),
+                ),
+            separatorBuilder: (context, index) => const Divider(
+                  height: 4,
+                  color: Colors.green,
+                ),
+            itemCount: dataRepo == null ? 0 : dataRepo.length),
       ),
     );
   }
